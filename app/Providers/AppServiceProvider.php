6f6e9bbc84882b28213adc7d6840514ce135a29a
Jordanie Njingang <jordannjingang1@gmail.com>
2026-09-09 08:12:08 +0100

    fix: enforce secure URLs in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.force_https')) {
+        if ($this->app->environment('production') || config('app.force_https')) {
             URL::forceScheme('https');
         }
 
